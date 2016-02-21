93f61a1bf5353a8b77951d46c6fcb57398fafb60
Viswanathan Manickam <Viswanathan Manickam@192.168.0.203>
2016-02-21 13:13:34 -0700

    'let' function logic implemented. Improvised logger usage Added test case

--- CLCalculator/src/main/java/Calculator.java
+++ CLCalculator/src/main/java/Calculator.java
@@ -4,6 +4,7 @@ import ca.clcalculator.exception.CLException;
 import java.util.HashMap;
 import java.util.Map;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.apache.log4j.Logger;
 
 /*
@@ -21,6 +22,8 @@ public class Calculator {
 
     private Map<String, Integer> commands = null;
 
+    private Map<String, Integer> variable = null;
+
     public Calculator() {
         commands = new HashMap<>();
         commands.put("add", 2);
@@ -31,11 +34,12 @@ public class Calculator {
     }
 
     private Boolean commandValid(String command) {
+        if (logger.isDebugEnabled()) {
+            logger.debug("Entering command valid check method");
+        }
         if (command == null) {
             return Boolean.FALSE;
         }
-
-//        command = command.substring(0, command.indexOf("("));
         if (commands.keySet().contains(command)) {
             return Boolean.TRUE;
         }
@@ -43,17 +47,42 @@ public class Calculator {
     }
 
     private Boolean checkParantheses(String command) {
+        if (logger.isDebugEnabled()) {
+            logger.debug("Entering paranthesis check method");
+        }
 
-        return Boolean.TRUE;
+        int count = 0;
+        for (int i = 0; i < command.length(); i++) {
+            if (command.charAt(i) == '(') {
+                count++;
+            } else if (command.charAt(i) == ')') {
+                count--;
+            }
+            if (count < 0) {
+                return Boolean.FALSE;
+            }
+        }
+        if (count == 0) {
+            return Boolean.TRUE;
+        }
+        return Boolean.FALSE;
     }
 
     private String getOperation(String command) throws CLException {
+        if (logger.isDebugEnabled()) {
+            logger.debug("Entering find operation method");
+        }
         if (command == null) {
             throw new CLException(CLException.COMMAND_ERROR);
         }
-
-        command =  StringUtils.substringBefore(command, "(");
+        if (logger.isInfoEnabled()) {
+            logger.info(command);
+        }
+        command = StringUtils.substringBefore(command, "(");
         if (commandValid(command)) {
+            if (logger.isDebugEnabled()) {
+                logger.debug("Exiting paranthesis check method");
+            }
             return command;
         } else {
             throw new CLException(CLException.COMMAND_ERROR);
@@ -61,80 +90,126 @@ public class Calculator {
     }
 
     private String[] getOperands(String input, Integer operandCount) {
+        if (logger.isDebugEnabled()) {
+            logger.debug("Entering finding operands method");
+        }
+        if (input == null || (input != null && input.equals(""))) {
+            return null;
+        }
         String[] result = null;
+        if (logger.isInfoEnabled()) {
+            logger.info("Trying to find operands from the input" + input);
+        }
+
         int count = StringUtils.countMatches(input, ",");
         if (count > operandCount - 1) {
+            result = new String[operandCount];
             int brackets = 0;
+            int index = 0;
             int foundMiddle = -1;
-            for (int i = 0; i < input.length(); i++) {
-                if (input.charAt(i) == '(') {
-                    brackets++;
-                } else if (input.charAt(i) == ')') {
-                    brackets--;
-                } else if (input.charAt(i) == ',' && brackets == 0) {
-                    foundMiddle = i;
-                    break;
+            int prevIndex = -1;
+            while (index <= operandCount - 1) {
+                for (int i = foundMiddle + 1; i < input.length(); i++) {
+                    if (input.charAt(i) == '(') {
+                        brackets++;
+                    } else if (input.charAt(i) == ')') {
+                        brackets--;
+                    } else if (input.charAt(i) == ',' && brackets == 0) {
+                        foundMiddle = i;
+                        break;
+                    }
                 }
+                if (index == 0) {
+                    result[index] = input.substring(0, foundMiddle);
+                } else if (index < operandCount - 1) {
+                    result[index] = input.substring(prevIndex + 1, foundMiddle);
+                } else {
+                    result[index] = input.substring(foundMiddle + 1, input.length());
+                }
+                prevIndex = foundMiddle;
+                index++;
             }
-            //System.out.println(foundMiddle);
-            result = new String[2];
-            result[0] = input.substring(0, foundMiddle);
-            result[1] = input.substring(foundMiddle + 1, input.length());
-            System.out.println(result[0]);
-            System.out.println(result[1]);
         } else {
             result = input.split(",");
-            /* result = new String[2];
-            result[0] = input.split(",")[0];
-            result[1] = input.split(",")[1];*/
+        }
+        if (logger.isDebugEnabled()) {
+            logger.debug("Exiting finding operands method");
         }
         return result;
     }
-    
-    
 
     private Integer evaluate(String input) throws CLException {
         if (logger.isDebugEnabled()) {
-            logger.debug("Entering evaluate method for input :" + input);
+            logger.debug("Entering evaluate method");
         }
         String operation = getOperation(input);
         if (logger.isInfoEnabled()) {
             logger.info("Found operation:" + operation);
         }
         String[] operands = null;
-        Integer[] operandValue = new Integer[2];
+        Integer[] operandValue = null;
         operands = getOperands(StringUtils.substring(StringUtils.substringAfter(input, "("), 0, StringUtils.substringAfter(input, "(").length() - 1), commands.get(operation));
         if (operands != null) {
+            operandValue = new Integer[operands.length];
+            if (operation.equalsIgnoreCase("let")) {
+                if (operands[0] != null && operands[1] != null) {
+                    if (isFunction(operands[1])) {
+                        variable.put(operands[0], evaluate(operands[1]));
+                    } else if (StringUtils.isNumeric(operands[1])) {
+                        variable.put(operands[0], Integer.parseInt(operands[1]));
+                    } else {
+                        throw new CLException(CLException.INTEGER_NUMBER_ERROR);
+                    }
+                }
+            }
+
             for (int i = 0; i < operands.length; i++) {
                 if (isFunction(operands[i])) {
                     operandValue[i] = evaluate(operands[i]);
-                } else {
+                } else if (StringUtils.isNumeric(operands[i])) {
                     operandValue[i] = Integer.parseInt(operands[i]);
+                } else if (variable.keySet().contains(operands[i])) {
+                    operandValue[i] = variable.get(operands[i]);
+                } else {
+                    throw new CLException(CLException.INTEGER_NUMBER_ERROR);
                 }
             }
+        } else {
+            throw new CLException(CLException.MISSING_PARAMETERS_ERROR);
         }
-        switch (operation) {
-            case "add":
-                return operandValue[0] + operandValue[1];
-            case "sub":
-                return operandValue[0] - operandValue[1];
-            case "mult":
-                return operandValue[0] * operandValue[1];
-            case "div":
-                try{
-                    return operandValue[0] / operandValue[1];
-                }catch(ArithmeticException e){
-                    throw new CLException(CLException.DIVIDE_ZERO_ERROR);
-                }
-            case "let":
-                
-                        
+        if (logger.isInfoEnabled()) {
+            for (Integer value : operandValue) {
+                logger.info("Found operands value:" + value);
+            }
+        }
+        try {
+            switch (operation) {
+                case "add":
+                    return operandValue[0] + operandValue[1];
+                case "sub":
+                    return operandValue[0] - operandValue[1];
+                case "mult":
+                    return operandValue[0] * operandValue[1];
+                case "div":
+                    try {
+                        return operandValue[0] / operandValue[1];
+                    } catch (ArithmeticException e) {
+                        throw new CLException(CLException.DIVIDE_ZERO_ERROR);
+                    }
+                case "let":
+                    return operandValue[2];
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new CLException(CLException.MISSING_PARAMETERS_ERROR);
         }
         return 0;
     }
 
-    private static Boolean isFunction(String operand) {
-        if (operand.contains("(")) {
+    private Boolean isFunction(String operand) {
+        if (logger.isDebugEnabled()) {
+            logger.debug("Checking whether the operand is a function");
+        }
+        if (operand != null && (operand.contains("(") || (commands.keySet().contains(operand)))) {
             return true;
         }
         return false;
@@ -144,10 +219,13 @@ public class Calculator {
         if (logger.isDebugEnabled()) {
             logger.debug("Started processing command");
         }
+        command = command.replaceAll("\\s", "");
         if (checkParantheses(command)) {
+            variable = new HashMap<>();
             return evaluate(command);
         } else {
             throw new CLException(CLException.PARANTHESIS_ERROR);
         }
+
     }
 }
--- CLCalculator/src/main/java/Main.java
+++ CLCalculator/src/main/java/Main.java
@@ -1,5 +1,7 @@
 
+import ca.clcalculator.exception.CLException;
 import java.util.Scanner;
+import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
 /*
@@ -15,25 +17,78 @@ public class Main {
 
     private static final Logger logger = Logger.getLogger(Main.class);
 
+    public static void printHelp() {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("\t Commands: \n\n");
+        builder.append("\t add(a,b): Add two integers\n");
+        builder.append("\t sub(a,b): Subtract two integers\n");
+        builder.append("\t mult(a,b): Multiply two integers \n");
+        builder.append("\t div(a,b): Divide two integers\n");
+        builder.append("\t let(a,value, operation): Assign variable with a value and execute the operation\n");
+        printOutput(builder.toString());
+    }
+
+    public static void changeLogger(String level) {
+
+        if (level != null) {
+            switch (level.toLowerCase()) {
+                case "error":
+                    Logger.getRootLogger().setLevel(Level.ERROR);
+                    break;
+                case "debug":
+                    Logger.getRootLogger().setLevel(Level.DEBUG);
+                    break;
+                case "info":
+                    Logger.getRootLogger().setLevel(Level.INFO);
+                    break;
+                default:
+                    Logger.getRootLogger().setLevel(Level.ERROR);
+                    break;
+            }
+            printOutput("Log level changed to " + level.toUpperCase());
+        }
+    }
+
+    public static void printOutput(String output) {
+        System.out.println("********************************************************************************");
+        System.out.println(output);
+        System.out.println("********************************************************************************");
+    }
+
     public static void main(String[] args) {
 
-        logger.debug("Starting CL Calculator application.....");
+        if (logger.isDebugEnabled()) {
+            logger.debug("Starting CL Calculator application.....");
+        }
+        
         System.out.println("********************************************************************************");
         System.out.println("Command Line Calculator");
-        System.out.println("Type 'h' for help");
+        System.out.println("Type 'h' or 'help' for help");
         System.out.println("********************************************************************************");
         Scanner in = new Scanner(System.in);
         while (true) {
             System.out.println(">");
-            String command = in.next();
+            String command = in.nextLine();
             Calculator c = new Calculator();
             try {
-                int result = c.process(command);
-                System.out.println("********************************************************************************");
-                System.out.println(command + " = " + result);
-                System.out.println("********************************************************************************");
+                if (command.contains("loglevel")) {
+                    try {
+                        String level = command.split(" ")[1];
+                        changeLogger(level);
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        throw new CLException(CLException.MISSING_PARAMETERS_ERROR);
+                    }
+
+                } else if (command.toLowerCase().equals("help") || command.toLowerCase().equals("h")) {
+                    printHelp();
+                } else {
+                    int result = c.process(command);
+                    printOutput(command + " = " + result);
+                }
             } catch (Exception e) {
-                logger.error(e);
+                logger.error(e.getMessage());
+                e.printStackTrace();
             }
         }
 
